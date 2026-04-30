6b9003941f1d01817f0289c700085593d66f35c7
Kevin Tilghman <55026237+tunerzk@users.noreply.github.com>
2026-04-30 18:29:02 -0400

    Add IAM role and policy attachment for Lambda

---
+++ IAM.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "chewbacca_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
